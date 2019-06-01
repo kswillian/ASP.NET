fc887ffb711f2bad1fd3986953e2cc6a8a78a301
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-06-01 16:39:13 -0300

    Criando o controlador juntamente com a estrutura de Insert, Update e Delete para a manipulação de dados

---
+++ Controllers/PratosController.cs
@@ -0,0 +1,123 @@
+﻿using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+using ASP.NET_Web_Application.Models;
+
+namespace ASP.NET_Web_Application.Controllers
+{
+    public class PratosController : Controller
+    {
+        private PratoDBContext db = new PratoDBContext();
+
+        // GET: Pratos
+        public ActionResult Index()
+        {
+            return View(db.Pratos.ToList());
+        }
+
+        // GET: Pratos/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prato prato = db.Pratos.Find(id);
+            if (prato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prato);
+        }
+
+        // GET: Pratos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Pratos/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,url_img,nome,descricao,valor")] Prato prato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Pratos.Add(prato);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(prato);
+        }
+
+        // GET: Pratos/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prato prato = db.Pratos.Find(id);
+            if (prato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prato);
+        }
+
+        // POST: Pratos/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,url_img,nome,descricao,valor")] Prato prato)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(prato).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(prato);
+        }
+
+        // GET: Pratos/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Prato prato = db.Pratos.Find(id);
+            if (prato == null)
+            {
+                return HttpNotFound();
+            }
+            return View(prato);
+        }
+
+        // POST: Pratos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Prato prato = db.Pratos.Find(id);
+            db.Pratos.Remove(prato);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
