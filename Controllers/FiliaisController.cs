430dea477c4afe6064b3af2a055097cc358b8b4b
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-06-01 16:44:48 -0300

    Criando o controlador juntamente com a estrutura de Insert, Update e Delete para a manipulação de dados

---
+++ Controllers/FiliaisController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ASP.NET_Web_Application.Models;
+
+namespace ASP.NET_Web_Application.Controllers
+{
+    public class FiliaisController : Controller
+    {
+        private FilialDBContext db = new FilialDBContext();
+
+        // GET: Filiais
+        public ActionResult Index()
+        {
+            return View(db.Filiais.ToList());
+        }
+
+        // GET: Filiais/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Filial filial = db.Filiais.Find(id);
+            if (filial == null)
+            {
+                return HttpNotFound();
+            }
+            return View(filial);
+        }
+
+        // GET: Filiais/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Filiais/Create
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,nome,cep,logradouro,bairro")] Filial filial)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Filiais.Add(filial);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(filial);
+        }
+
+        // GET: Filiais/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Filial filial = db.Filiais.Find(id);
+            if (filial == null)
+            {
+                return HttpNotFound();
+            }
+            return View(filial);
+        }
+
+        // POST: Filiais/Edit/5
+        // Para se proteger de mais ataques, ative as propriedades específicas a que você quer se conectar. Para 
+        // obter mais detalhes, consulte https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,nome,cep,logradouro,bairro")] Filial filial)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(filial).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(filial);
+        }
+
+        // GET: Filiais/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Filial filial = db.Filiais.Find(id);
+            if (filial == null)
+            {
+                return HttpNotFound();
+            }
+            return View(filial);
+        }
+
+        // POST: Filiais/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Filial filial = db.Filiais.Find(id);
+            db.Filiais.Remove(filial);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
