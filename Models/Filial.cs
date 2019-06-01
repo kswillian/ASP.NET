12796d12818d44837b054488a1ddd210f3265d26
Willian Kaminski dos Santos <developer.williansantos@gmail.com>
2019-06-01 16:16:40 -0300

    Criando as clases de modelo para a representação na base dados

---
+++ Models/Filial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace ASP.NET_Web_Application.Models
+{
+    public class Filial
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string cep { get; set; }
+        public string logradouro { get; set; }
+        public string bairro { get; set; }
+        public char uf { get; set; }
+    }
+
+    public class FilialDBContext : DbContext
+    {
+        public DbSet<Filial> Filiais { get; set; }
+    }
+}
---
+++ Models/Prato.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace ASP.NET_Web_Application.Models
+{
+    public class Prato
+    {
+        public int id { get; set; }
+        public string url_img { get; set; }
+        public string nome { get; set; }
+        public string descricao { get; set; }
+        public double valor { get; set; }
+    }
+
+    public class PratoDBContext : DbContext
+    {
+        public DbSet<Prato> Pratos { get; set; }
+    }
+}
